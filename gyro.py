8a8e8f370f914a85c5b58d2ee83cb0047f83063c
Anshuman-UCSB <anshuman@ucsb.edu>
2021-01-01 17:07:44 -0800

    Mpu6050

--- gyro.py
+++ gyro.py
@@ -1,22 +1,10 @@
 from mpu6050 import mpu6050
-from time import sleep
+from time import *
+import requests
 
 sensor = mpu6050(0x68)
 
 while True:
-    accel_data = sensor.get_accel_data()
     gyro_data = sensor.get_gyro_data()
-    temp = sensor.get_temp()
-
-    print("Accelerometer data")
-    print("x: " + str(accel_data['x']))
-    print("y: " + str(accel_data['y']))
-    print("z: " + str(accel_data['z']))
-
-    print("Gyroscope data")
     print("x: " + str(gyro_data['x']))
-    print("y: " + str(gyro_data['y']))
-    print("z: " + str(gyro_data['z']))
-
-    print("Temp: " + str(temp) + " C")
-    sleep(0.5)
+    requests.post("http://127.0.0.1:5000/", datta = {'x':time(), 'y':gyro_data['x']})
