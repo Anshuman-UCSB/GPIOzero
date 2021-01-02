4e9e5ba29a92135f04356a8c74c5b4bf6a103a6e
Anshuman-UCSB <anshuman@ucsb.edu>
2021-01-01 17:56:28 -0800

    Updated request structure

--- gyro.py
+++ gyro.py
@@ -7,4 +7,4 @@ sensor = mpu6050(0x68)
 while True:
     gyro_data = sensor.get_gyro_data()
     print("x: " + str(gyro_data['x']))
-    requests.post("http://192.168.0.34:5000/", data = {'x':time(), 'y':gyro_data['x']})
+    requests.post("http://192.168.0.34:5000/", data = f"{time()},{gyro_data['x']}")
